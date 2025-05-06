c71a18be46b537be3232c449f1bf79dacc8843b4
Pedro <colcapedro@gmail.com>
2025-05-06 17:25:58 -0500

    feat: :sparkles: Se comenzo a generar la estructura del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  // { path: 'home', component: HomeComponent },
+  // { path: '**', redirectTo: '/login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
