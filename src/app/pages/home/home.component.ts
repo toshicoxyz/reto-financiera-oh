f8cdfed20b034ba2bfb40e002cdd0f1e7d6b41c5
Pedro <colcapedro@gmail.com>
2025-05-06 17:47:19 -0500

    feat: :sparkles: Segunda etapa :)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  accordionOpen = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleAccordion() {
+    this.accordionOpen = !this.accordionOpen;
   }
-
 }
