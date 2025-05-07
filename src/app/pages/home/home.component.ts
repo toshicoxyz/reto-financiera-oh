45a5d0050c551632b4dd4d864ef3039e27695426
Pedro <colcapedro@gmail.com>
2025-05-06 22:37:50 -0500

    feat: :sparkles: Final de etapa estructura basica

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,17 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   accordionOpen = false;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
   toggleAccordion() {
     this.accordionOpen = !this.accordionOpen;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
